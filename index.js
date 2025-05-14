860547b2d2ce39799bd218c89aca137f9b476f06
essa2004 <154955315+essa2004@users.noreply.github.com>
2025-05-14 13:59:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
 // إنشاء الخادم وتوجيه الطلبات إلى الهدف المحدد
 http.createServer((req, res) => {
-proxy.web(req, res, { target: 'https://www.google.com' });
+proxy.web(req, res, { target: 'https://www.pornhub.com' });
 }).listen(3000, () => {
 console.log('Proxy running at http://localhost:3000');
 });
