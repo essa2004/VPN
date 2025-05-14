6106b9e94d331f4a8127508459271231da8c5296
essa2004 <154955315+essa2004@users.noreply.github.com>
2025-05-14 13:34:28 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
 // إنشاء الخادم وتوجيه الطلبات إلى الهدف المحدد
 http.createServer((req, res) => {
-const url = req.params.url;
-console.log(url)
 proxy.web(req, res, { target: 'https://www.google.com' });
 }).listen(3000, () => {
 console.log('Proxy running at http://localhost:3000');
