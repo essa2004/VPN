09f2964542afc89176ef90d85b85c3ac82e9664d
essa2004 <154955315+essa2004@users.noreply.github.com>
2025-05-14 13:32:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 const http = require('http');
-const url = require('url');
 const httpProxy = require('http-proxy');
 
 // إنشاء البروكسي
 const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
-// ذاكرة لتخزين آخر URL تم استخدامه
-let lastUsedUrl = null;
-
-// إنشاء الخادم
+// إنشاء الخادم وتوجيه الطلبات إلى الهدف المحدد
 http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const query = parsedUrl.query;
-
-    // إذا وُجد URL جديد يتم تخزينه
-    if (query.url) {
-        lastUsedUrl = query.url;
-        console.log(`تم تحديث العنوان إلى: ${lastUsedUrl}`);
-    }
-
-    // إذا لم يتم إرسال URL ولكن لدينا عنوان سابق، نستخدمه
-    if (lastUsedUrl) {
-        proxy.web(req, res, { target: `https://${lastUsedUrl}` });
-    } else {
-        // لا يوجد عنوان جديد ولا قديم، نكمل الاتصال بصمت
-        res.writeHead(204); // No Content
-        res.end();
-    }
+const url = req.params.url;
+console.log(url)
+proxy.web(req, res, { target: 'https://www.google.com' });
 }).listen(3000, () => {
-    console.log('Proxy running at http://localhost:3000');
+console.log('Proxy running at http://localhost:3000');
 });
