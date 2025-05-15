b4a5dfbe720d13cda67b6cd99e52d807e31567c6
essa2004 <154955315+essa2004@users.noreply.github.com>
2025-05-16 01:15:48 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const socks = require('socksv5');
+const TelegramBot = require('node-telegram-bot-api');
 
-socks.createServer((info, accept, deny) => {
-    console.log('xxxxxxx');
-  accept();
-}).listen(1080, '0.0.0.0', () => {
-  console.log('SOCKS5 proxy server listening on port 1080');
-}).useAuth(socks.auth.None());
+// ضع توكن البوت الخاص بك هنا
+const token = '6905090609:AAErK2lotBCgHExm1MZAQjOf9fdR9PBhEyU';
+
+// أنشئ البوت
+const bot = new TelegramBot(token, { polling: true });
+
+// عند الضغط على /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const firstName = msg.from.first_name || 'صديقي';
+
+  bot.sendMessage(chatId, `مرحباً ${firstName}! كيف يمكنني مساعدتك؟`);
+});
